4fffe871c658650afea0245bc140e23a5b58a686
jkeslin <jkeslin@gmail.com>
2015-05-14 22:34:57 -0500

    added some answers to tuls' questions

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -44,10 +44,15 @@ def build_array_from_range(min, max)
   (min..max).to_a
 end
 #to_a makes a new array, right??
+# => Yes
+
 #does this convention allow for more quick changing of the parameters or why the min, max vs. 1, 100>
+# => I went with using min and max as arguments so that I could easily run the program using the numbers of my choice instead of just 1 and 100.
 
 #ruby convention: if method is designed to return a boolean, end it with a question mark
 #this makes sense.  i think i thought when i saw ? in previous lessons they were required for the method to work - but it's simply a convention?
+# =>simply a convention, yes.  The way you used the "?" in your solution was different however.  that was a ternary operator, which is a nice way of writing an if/else statement in one line of code. Here's a nice little tutorial on that subject: http://rubylearning.com/satishtalim/simple_constructs.html
+
 def replace_with_fizz?(integer)
   integer % 3 == 0
 end
@@ -60,6 +65,7 @@ def replace_with_fizzbuzz?(integer)
   integer % 3 == 0 && integer % 5 == 0
 end
 #why is it better to make each of these their own defined method?
+# =>it makes the code easier to maintain and edit and reuse in other places.  Not at all necessary, for something like fizzbuzz, but a good habit for when things get more complicated.  It's like if you had design drawings for a house.  It's easier to work with if the plumbing schematic is on one page, and the electrical is on another, and the heating/AC is on another page, etc. 
 
 def check_for_replacement(integers_array)
   integers_array.map! do |integer|
@@ -72,13 +78,15 @@ def check_for_replacement(integers_array)
   integers_array
 end
 #generally should I use if, elseif, else rather than : and ?
+# =>totally up to you, I just like the ifs and thens cause it forms a sentence-ish.
 #did you have to define integers_array or if you put _array will it automatically make it an array
+# =>integers_array could have been called anything.  what matters is that when I CALL the check_for_replacement method (like in like 93) that the argument I give it is an array
 
 def print_fizzbuzzed(array)
   puts array
 end
 #why is this definition here?
-
+# => it's not really necessary, again it's really just overkill on trying to demonstrate readability 
 
 #this method is the runner, so to speak
 def solve_fizzbuzz(min, max)
@@ -101,9 +109,10 @@ solve_fizzbuzz(1, 100_000)
 #   bm.report { megs_approach }
 # end
 
-
-
-
-
+#TRYING to get a random integers? Try:
+#rand(6) + 1
+#or
+# Random.new.rand(5) + 1
+#or something like that.
 
 
