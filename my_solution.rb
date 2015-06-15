1c917fab18b4c00db3531039aa69232bb9bf19d8
Meg Tuls <meglktuls@gmail.com>
2015-06-14 22:36:01 -0500

    Share my_solution file for DBC on creating 4-person groups

---
+++ my_solution.rb
@@ -0,0 +1,34 @@
+# U2.W4: Create Accountability Groups
+
+# I worked on this challenge [by myself, with: ]
+
+# I spent [] hours on this challenge.
+
+# 0. Pseudocode
+
+# Input: 
+# => An array of names
+# Output: 
+# => The same names divided into groups of 4
+# => Arrays that contain 4's OR hashes that assign a name to a group number
+# Steps:
+# Define method with 1 argument that is an array of names
+# Shuffle array to create randomized cohorts
+# Find array count to determine number of groups
+# Build nested array with 
+
+# 1. Initial Solution
+
+
+
+
+# 2. Refactored Solution
+
+
+
+
+
+
+# 3. Reflection
+
+
